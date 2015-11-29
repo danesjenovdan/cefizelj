f3c9e81a9da98879bf468e19a0e06ad3a93a3047
Filip Muki Dobranic <filipdobranic@gmail.com>
2015-11-29 12:08:33 +0100

    finished content for gays

---
+++ js/archive/gaycontent.js
@@ -0,0 +1,169 @@
+var gaytree = {
+  'tree': [{
+    '_id': '0',
+    'title': 'Želim glasovati ZA, toda ...',
+    'name': 'Želim glasovati ZA, toda ...',
+    'type': 'menu',
+    'items': [{
+      '_id': '1',
+      'title': '... na dan referenduma me ne bo doma.',
+      'name': '... na dan referenduma me ne bo doma.',
+      'type': 'menu',
+      'items': [{
+        '_id': '3',
+        'title': 'Na dan referenduma bom v tujini.',
+        'name': 'Na dan referenduma bom v tujini.',
+        'type': 'menu',
+        'items': [{
+          '_id': '5',
+          'title': 'V tujini imam stalno prebivališče.',
+          'name': 'V tujini imam stalno prebivališče.',
+          'type': 'menu',
+          'items': [{
+            '_id': '25',
+            'title': 'V tujini imam stalno prebivališče.',
+            'name': 'V tujini imam stalno prebivališče.',
+            'type': 'page',
+            'content': '<div class="htmlcontainer"> <div class="visible-xs centermycontentvertically nazajcontainer"> <div class="centermevertically nazaj bck">Nazaj</div></div><h1>Kako lahko glasujem?</h1> <p>Glasujem lahko po pošti ali na <a href="http://www.dvk-rs.si/files/files/sklep-o-dolocitvi-volisc-na-dkp1.pdf">diplomatsko-konzularnem predstavništvu</a>.</p><h3>Kaj moram storiti?</h3> <p>1. <a href="http://www.dvk-rs.si/files/files/Obr01_glasovanje_po_posti_izseljenci.pdf" target="_blank">Namero (link)</a> posredujem državni volilni komisiji do <strong>4.12. (Državna volilna komisija mora obvestilo volivca prejeti najkasneje do 4. 12. 2015 do 24. ure)</strong> </p><ul> <li>po e-pošti (skenirano zaradi podpisa) na e-naslov gp.dvk@dvk-rs.si </li><li>po faksu na številko: 00386 143 31 269 </li><li>na naslov: Državna volilna komisija, Slovenska cesta 54, SI-1000 Ljubljana, Slovenija </li></ul> <p>2. Na obrazcu je potrebno označiti način glasovanja in sicer:</p><ul> <li><strong>a - Glasujem na <a href="http://www.dvk-rs.si/files/files/sklep-o-dolocitvi-volisc-na-dkp1.pdf">diplomatsko-konzularnem predstavništvu</a>.</strong></li><li><strong>b - Glasujem po pošti iz tujine.</strong></li></ul> </div>',
+            'items': []
+          }]
+        }, {
+          '_id': '6',
+          'title': 'V tujini bom začasno.',
+          'name': 'V tujini bom začasno.',
+          'type': 'menu',
+          'items': [{
+            '_id': '24',
+            'title': 'V tujini bom začasno.',
+            'name': 'V tujini bom začasno.',
+            'type': 'page',
+            'content': '<div class="htmlcontainer"> <div class="visible-xs centermycontentvertically nazajcontainer"> <div class="centermevertically nazaj bck">Nazaj</div></div><h1>Kako lahko glasujem?</h1> <p>Glasujem lahko po pošti ali na <a href="http://www.dvk-rs.si/files/files/sklep-o-dolocitvi-volisc-na-dkp1.pdf">diplomatsko-konzularnem predstavništvu</a>.</p><h3>Kaj moram storiti?</h3> <p>1. <a href="http://www.dvk-rs.si/files/files/Obr02_glasovanje_po_posti_zdomci.pdf" target="_blank">Namero (link)</a> posredujem državni volilni komisiji do <strong>4.12. (Državna volilna komisija mora obvestilo volivca prejeti najkasneje do 4. 12. 2015 do 24. ure)</strong> </p><ul> <li>po e-pošti (skenirano zaradi podpisa) na e-naslov gp.dvk@dvk-rs.si </li><li>po faksu na številko: 00386 143 31 269 </li><li>na naslov: Državna volilna komisija, Slovenska cesta 54, SI-1000 Ljubljana, Slovenija </li></ul> <p>2. Na obrazcu je potrebno označiti način glasovanja in sicer:</p><ul> <li><strong>a - Glasujem na <a href="http://www.dvk-rs.si/files/files/sklep-o-dolocitvi-volisc-na-dkp1.pdf">diplomatsko-konzularnem predstavništvu</a>.</strong></li><li><strong>b - Glasujem po pošti iz tujine.</strong></li></ul> </div>',
+            'items': []
+          }]
+        }]
+      }, {
+        '_id': '4',
+        'title': 'Na dan referenduma bom v Sloveniji.',
+        'name': 'Na dan referenduma bom v Sloveniji.',
+        'type': 'menu',
+        'items': [{
+          '_id': '7',
+          'title': 'Imam stalno prebivališče v tujini.',
+          'name': 'Imam stalno prebivališče v tujini.',
+          'type': 'menu',
+          'items': [{
+            '_id': '23',
+            'title': 'Imam stalno prebivališče v tujini.',
+            'name': 'Imam stalno prebivališče v tujini.',
+            'type': 'page',
+            'content': '<div class="htmlcontainer"><div class="visible-xs centermycontentvertically nazajcontainer"> <div class="centermevertically nazaj bck">Nazaj</div></div><h1>Kako lahko glasujem?</h1> <p>Glasujem lahko na posebnem volišču na sedežu okrajne volilne komisije (upravne enote). <a href="http://www.dvk-rs.si/files/files/sedezi-OMNIA---DZ-2014.pdf" target="_blank">Volišča "OMNIA"</a> so namenjena volivcem zunaj okraja svojega stalnega prebivališča.</p><h3>Kaj moram storiti?</h3> <p>1. <a href="http://www.dvk-rs.si/files/files/Obr03_glasovanje_na_voliscu_izseljenci.pdf" target="_blank">Namero (link)</a> posredujem okrajni ali državni volilni komisiji do <strong>16.12. (Državna volilna komisija mora obvestilo volivca prejeti najkasneje do 16. 12. 2015 do 24. ure)</strong> </p><ul> <li>po e-pošti (skenirano zaradi podpisa) na e-naslov gp.dvk@dvk-rs.si </li><li>po faksu na številko: 00386 143 31 269 </li><li>na naslov: Državna volilna komisija, Slovenska cesta 54, SI-1000 Ljubljana, Slovenija </li></ul></div>',
+            'items': []
+          }]
+        }, {
+          '_id': '8',
+          'title': 'Na dan referenduma bom izven kraja stalnega prebivališča.',
+          'name': 'Na dan referenduma bom izven kraja stalnega prebivališča.',
+          'type': 'menu',
+          'items': [{
+            '_id': '22',
+            'title': 'Na dan referenduma bom izven kraja stalnega prebivališča.',
+            'name': 'Na dan referenduma bom izven kraja stalnega prebivališča.',
+            'type': 'page',
+            'content': '<div class="htmlcontainer"> <div class="visible-xs centermycontentvertically nazajcontainer"> <div class="centermevertically nazaj bck">Nazaj</div></div><h1>Kako lahko glasujem?</h1> <h2>1. Glasujem lahko na posebnem volišču</h2> <p>Glasujem lahko na posebnem volišču na sedežu okrajne volilne komisije (upravne enote). <a href="http://www.dvk-rs.si/files/files/sedezi-OMNIA---DZ-2014.pdf" target="_blank">Volišča "OMNIA"</a> so namenjena volivcem zunaj okraja svojega stalnega prebivališča.</p><h3>Kaj moram storiti?</h3> <p>1. <a href="http://www.dvk-rs.si/files/files/Obr05_glasovanje_izven_okraja.pdf" target="_blank">Namero (link)</a> posredujem okrajni ali državni volilni komisiji do <strong>16.12. (Državna volilna komisija mora obvestilo volivca prejeti najkasneje do 16. 12. 2015 do 24. ure)</strong> </p><ul> <li>po e-pošti (skenirano zaradi podpisa) na e-naslov gp.dvk@dvk-rs.si </li><li>po faksu na številko: 00386 143 31 269 </li><li>na naslov: Državna volilna komisija, Slovenska cesta 54, SI-1000 Ljubljana, Slovenija </li></ul> <h2>2. Glasujem lahko predčasno.</h2> <p>To lahko storim samo na tisti okrajni volilni komisiji, na območju katere sem vpisan v volilni imenik (kjer imam prijavljeno stalno prebivališče). Na volišču se zglasim brez predhodne najave ali izpolnjevanja obrazcev.</p><p><strong>Na okrajni volilni komisiji se lahko zglasim od torka, 15.12. do četrtka, 17.12., med 7. in 19. uro.</strong></p></div>',
+            'items': []
+          }]
+        }, {
+          '_id': '9',
+          'title': 'Na dan referenduma bom v zavodu za izvrševanje kazenskih sankcij.',
+          'name': 'Na dan referenduma bom v zavodu za izvrševanje kazenskih sankcij.',
+          'type': 'menu',
+          'items': [{
+            '_id': '21',
+            'title': 'Na dan referenduma bom v zavodu za izvrševanje kazenskih sankcij.',
+            'name': 'Na dan referenduma bom v zavodu za izvrševanje kazenskih sankcij.',
+            'type': 'page',
+            'content': '<div class="htmlcontainer"> <div class="visible-xs centermycontentvertically nazajcontainer"> <div class="centermevertically nazaj bck">Nazaj</div></div><h1>Kako lahko glasujem?</h1> <h2>Glasujem lahko po pošti.</h2> <p>1. <a href="http://www.dvk-rs.si/files/files/Obr04_glasovanje_po_posti_slovenija.pdf" target="_blank">Namero (link)</a> posredujem državni volilni komisiji do <strong>9.12. (Državna volilna komisija mora obvestilo volivca prejeti najkasneje do 9.12.2015 do 24. ure)</strong> </p><ul> <li>po e-pošti (skenirano zaradi podpisa) na e-naslov gp.dvk@dvk-rs.si </li><li>po faksu na številko: 00386 143 31 269 </li><li>na naslov: Državna volilna komisija, Slovenska cesta 54, SI-1000 Ljubljana, Slovenija </li></ul></div>',
+            'items': []
+          }]
+        }]
+      }]
+    }, {
+      '_id': '2',
+      'title': '... volišče mi ni dostopno.',
+      'name': '... volišče mi ni dostopno.',
+      'type': 'menu',
+      'items': [{
+        '_id': '10',
+        'title': 'Zaradi bolezni ne morem od doma.',
+        'name': 'Zaradi bolezni ne morem od doma.',
+        'type': 'menu',
+        'items': [{
+          '_id': '20',
+          'title': 'Zaradi bolezni ne morem od doma.',
+          'name': 'Zaradi bolezni ne morem od doma.',
+          'type': 'page',
+          'content': '<div class="htmlcontainer"> <div class="visible-xs centermycontentvertically nazajcontainer"> <div class="centermevertically nazaj bck">Nazaj</div></div><h1>Kako lahko glasujem?</h1> <h2>Glasujem lahko po pošti.</h2> <p>1. <a href="http://www.dvk-rs.si/files/files/Obr07_glasovanje_na_domu.pdf" target="_blank">Namero (link)</a> posredujem državni volilni komisiji do <strong>16.12. (Državna volilna komisija mora obvestilo volivca prejeti najkasneje do 3 (tri) dni pred glasovanjem.)</strong> </p><p> <strong>Glasovanje opravim pred volilnim odborom, ki me obišče na domu.</strong> </p></div>',
+          'items': []
+        }]
+      }, {
+        '_id': '11',
+        'title': 'Sem v bolnišnici ali zdravilišču.',
+        'name': 'Sem v bolnišnici ali zdravilišču.',
+        'type': 'menu',
+        'items': [{
+          '_id': '19',
+          'title': 'Sem v bolnišnici ali zdravilišču.',
+          'name': 'Sem v bolnišnici ali zdravilišču.',
+          'type': 'page',
+          'content': '<div class="htmlcontainer"> <div class="visible-xs centermycontentvertically nazajcontainer"> <div class="centermevertically nazaj bck">Nazaj</div></div><h1>Kako lahko glasujem?</h1> <h2>Glasujem lahko po pošti.</h2> <p>1. <a href="http://www.dvk-rs.si/files/files/Obr04_glasovanje_po_posti_slovenija.pdf" target="_blank">Namero (link)</a> posredujem državni volilni komisiji do <strong>9.12. (Državna volilna komisija mora obvestilo volivca prejeti najkasneje do 9.12.2015 do 24. ure)</strong> </p><ul> <li>po e-pošti (skenirano zaradi podpisa) na e-naslov gp.dvk@dvk-rs.si </li><li>po faksu na številko: 00386 143 31 269 </li><li>na naslov: Državna volilna komisija, Slovenska cesta 54, SI-1000 Ljubljana, Slovenija </li></ul></div>',
+          'items': []
+        }]
+      }, {
+        '_id': '12',
+        'title': 'Ovira me hendikep.',
+        'name': 'Ovira me hendikep.',
+        'type': 'menu',
+        'items': [{
+          '_id': '14',
+          'title': 'Sem gibalno oviran/-a.',
+          'name': 'Sem gibalno oviran/-a.',
+          'type': 'menu',
+          'items': [{
+            '_id': '16',
+            'title': 'Sem gibalno oviran/-a.',
+            'name': 'Sem gibalno oviran/-a.',
+            'type': 'page',
+            'content': '<div class="htmlcontainer"> <div class="visible-xs centermycontentvertically nazajcontainer"> <div class="centermevertically nazaj bck">Nazaj</div></div><h1>Kako lahko glasujem?</h1> <h2>Glasujem lahko po pošti.</h2> <p>1. <a href="http://www.dvk-rs.si/files/files/Obr06_glasovanje_na_voliscu_dostopno_invalidom.pdf" target="_blank">Namero (link)</a> posredujem državni volilni komisiji do <strong>16.12. (Državna volilna komisija mora obvestilo volivca prejeti najkasneje 3 (tri) dni pred glasovanjem.</strong> </p><ul> <li>po e-pošti (skenirano zaradi podpisa) na e-naslov gp.dvk@dvk-rs.si </li><li>po faksu na številko: 00386 143 31 269 </li><li>na naslov: Državna volilna komisija, Slovenska cesta 54, SI-1000 Ljubljana, Slovenija </li></ul></div>',
+            'items': []
+          }]
+        }, {
+          '_id': '15',
+          'title': 'Sem senzorno oviran/-a.',
+          'name': 'Sem senzorno oviran/-a.',
+          'type': 'menu',
+          'items': [{
+            '_id': '16',
+            'title': 'Sem senzorno oviran/-a.',
+            'name': 'Sem senzorno oviran/-a.',
+            'type': 'page',
+            'content': '<div class="htmlcontainer"> <div class="visible-xs centermycontentvertically nazajcontainer"> <div class="centermevertically nazaj bck">Nazaj</div></div><h1>Kako lahko glasujem?</h1> <h2>Glasujem lahko po pošti.</h2> <p>1. <a href="http://www.dvk-rs.si/files/files/Obr06_glasovanje_na_voliscu_dostopno_invalidom.pdf" target="_blank">Namero (link)</a> posredujem državni volilni komisiji do <strong>16.12. (Državna volilna komisija mora obvestilo volivca prejeti najkasneje 3 (tri) dni pred glasovanjem.</strong> </p><ul> <li>po e-pošti (skenirano zaradi podpisa) na e-naslov gp.dvk@dvk-rs.si </li><li>po faksu na številko: 00386 143 31 269 </li><li>na naslov: Državna volilna komisija, Slovenska cesta 54, SI-1000 Ljubljana, Slovenija </li></ul></div>',
+            'items': []
+          }]
+        }]
+      }, {
+        '_id': '13',
+        'title': 'Bivam v domu za starejše, ki ni moje stalno prebivališče.',
+        'name': 'Bivam v domu za starejše, ki ni moje stalno prebivališče.',
+        'type': 'menu',
+        'items': [{
+          '_id': '18',
+          'title': 'Živim v domu za starejše.',
+          'name': 'Živim v domu za starejše.',
+          'type': 'page',
+          'content': '<div class="htmlcontainer"> <div class="visible-xs centermycontentvertically nazajcontainer"> <div class="centermevertically nazaj bck">Nazaj</div></div><h1>Kako lahko glasujem?</h1> <h2>Glasujem lahko po pošti.</h2> <p>1. <a href="http://www.dvk-rs.si/files/files/Obr04_glasovanje_po_posti_slovenija.pdf" target="_blank">Namero (link)</a> posredujem državni volilni komisiji do <strong>9.12. (Državna volilna komisija mora obvestilo volivca prejeti najkasneje do 9.12.2015 do 24. ure)</strong> </p><ul> <li>po e-pošti (skenirano zaradi podpisa) na e-naslov gp.dvk@dvk-rs.si </li><li>po faksu na številko: 00386 143 31 269 </li><li>na naslov: Državna volilna komisija, Slovenska cesta 54, SI-1000 Ljubljana, Slovenija </li></ul></div>',
+          'items': []
+        }]
+      }]
+    }]
+  }]
+}
