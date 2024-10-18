176e745519b6389b6d42cb7f12f73fc99de50566
LepkoQQ <lepko.san@gmail.com>
2024-10-18 12:27:45 +0200

    hide help button if missing help html but has article defined

--- src/check_html_files_in_tree.mjs
+++ src/check_html_files_in_tree.mjs
@@ -8,21 +8,36 @@ const json = JSON.parse(fs.readFileSync(jsonFile, "utf8"));
 
 const tree = json.tree;
 
+const allFiles = new Set(fs.readdirSync(path.resolve("../public/pages")));
+allFiles.delete("Slike");
+
+const foundFiles = new Set();
+
 function checkIfFilesExist(node) {
   const keys = ["html", "more", "article", "help"];
 
   for (let key of keys) {
     if (node[key]) {
-      const file = path.resolve(`../public/pages/${node[key]}`);
-      if (!fs.existsSync(file)) {
-        let fileToRename = path.resolve(
-          `../public/pages/${node[key].replace(".html", ".HTML")}`
-        );
-        if (fs.existsSync(fileToRename)) {
-          console.log("rename", fileToRename, "to", file);
-          // fs.renameSync(fileToRename, file);
+      if (!foundFiles.has(node[key])) {
+        foundFiles.add(node[key]);
+      } else {
+        console.error(`Duplicate file: ${node[key]}`);
+      }
+
+      if (!allFiles.has(node[key])) {
+        let fileToRename = node[key].replace(".html", ".HTML");
+        if (allFiles.has(fileToRename)) {
+          console.log("Rename", fileToRename, "to", node[key]);
+          // fs.renameSync(
+          //   path.resolve("../public/pages", fileToRename),
+          //   path.resolve("../public/pages", node[key])
+          // );
+        } else if (key === "help" && node["article"]) {
+          console.error(`Help not found but has article defined: ${node[key]}`);
+          // node[`__${key}`] = node[key];
+          // node[key] = undefined;
         } else {
-          console.error(`File not found (${key}): ${file}`);
+          console.error(`File not found (${key}): ${node[key]}`);
         }
       }
     }
@@ -40,3 +55,17 @@ function checkNode(node) {
 }
 
 checkNode(tree);
+
+fs.writeFileSync(jsonFile, JSON.stringify(json, null, 2));
+
+// for (let file of allFiles) {
+//   if (!foundFiles.has(file)) {
+//     console.error(`Extra file: ${file}`);
+//   }
+// }
+
+// for (let file of foundFiles) {
+//   if (!allFiles.has(file)) {
+//     console.error(`Missing file: ${file}`);
+//   }
+// }
